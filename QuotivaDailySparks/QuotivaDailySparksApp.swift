fbf6e5f7ebe1149f4a5c34fbaa8e0f559c777e84
Jeff Deng <dengh@oregonstate.edu>
2023-05-20 01:46:06 -0700

    added README

---
+++ QuotivaDailySparks/QuotivaDailySparksApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotivaDailySparksApp.swift
+//  QuotivaDailySparks
+//
+//  Created by Jeff Deng on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct QuotivaDailySparksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Daily quotes and facts ios app
